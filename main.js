afa38ab7eb99f3d3ce4210e8cdf9f6f746fce188
Daylton Douglas <daylton2000@gmail.com>
2021-11-06 10:37:09 -0300

    Adição de animação e de moldagem correta do modal

--- main.js
+++ main.js
@@ -5,15 +5,11 @@ function setButtonFunctions() {
     method: "GET",
     headers: {
       "x-rapidapi-host": "covid-193.p.rapidapi.com",
-      "x-rapidapi-key": "b7c2a7cf8amsh1fabb237102b82ap1f049fjsn94310bffa79d",
+      "x-rapidapi-key": "2dc2b1b91emsh77d8ce735fe22bep1a7a8ajsnf768300d3462",
     },
   })
     .then((response) => response.json())
     .then((response) => {
-      console.log("COVID 19 API object:");
-      console.log(response);
-      console.log("\n");
-
       // add all countries to select element
       response.response.forEach((c) => {
         const option = document.createElement("option");
@@ -21,7 +17,6 @@ function setButtonFunctions() {
         document.getElementById("countries").appendChild(option);
       });
 
-      // save covid data to global variable
       covid19data = response.response;
       document.getElementById("countries").onchange = function () {
         const selectedValue = document.getElementById("countries").value;
@@ -29,7 +24,6 @@ function setButtonFunctions() {
           (c) => c.country == selectedValue
         )[0];
         console.log(countryData);
-        // display data
         const newConfirmed = document.getElementById("covidNewConfirmed");
         const totalConfirmed = document.getElementById("covidTotalConfirmed");
         const covidNewDeaths = document.getElementById("covidNewDeaths");
@@ -61,12 +55,19 @@ function setButtonFunctions() {
 
 function abrir() {
   let modal = document.querySelector(".modal-container");
+  let nav = document.querySelector(".nav");
+
+  nav.style.display = "none";
 
   modal.style.display = "block";
 }
 function fechar() {
   let modal = document.querySelector(".modal-container");
+  let nav = document.querySelector(".nav");
 
+  nav.style.display = "inline-block";
+  nav.style.top = "120px";
+  nav.style.left = "100px";
   modal.style.display = "none";
 }
 setButtonFunctions();
